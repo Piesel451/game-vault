932dae8521d1d45110851c64559c86adbfccb59e
Mateusz Wojtyczka <mateusz.wojtyczka044@gmail.com>
2026-02-17 13:16:46 +0100

    feat: initial project structure with game and review models

---
+++ README.md
@@ -0,0 +1,3 @@
+# GameVault
+
+GameVault is a web application for managing a video game database and user reviews. This project is currently in development as part of a backend learning path.
---
+++ games/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    developer = models.CharField(max_length=100)
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='reviews')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Recenzja {self.game.title} autorstwa {self.author}"
