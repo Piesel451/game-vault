9a63d74d68f960b3e4a347df8c5f38d771acb58a
Mateusz Wojtyczka <mateusz.wojtyczka044@gmail.com>
2026-02-27 23:33:53 +0100

    feat(users): add user registration view and template

--- games/urls.py
+++ games/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.games_list, name='games_list'),
-    path('review/<int:pk>', views.game_reviews, name='game_reviews')
+    path('review/<int:pk>/', views.game_reviews, name='game_reviews')
 ]
--- games/views.py
+++ games/views.py
@@ -5,6 +5,6 @@ def games_list(request):
     games = Game.objects.all()
     return render(request, 'games/games_list.html', {'games': games})
 
-def game_reviews(request, pk): #ISSUE: 1 zapytanie o listę recenzji + X zapytań o każdego autora z osobna  lepiej zrobic joina czylis select_related()
+def game_reviews(request, pk):
     reviews = Review.objects.filter(game_id = pk).select_related("author")
     return render(request, 'games/review_list.html', {'reviews': reviews})
